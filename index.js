55e741f6cdd6dc2684dc9ba6a8e9c2342ea037b3
dheena <mytillygiri98@gmail.com>
2023-06-02 11:02:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server started on http://localhost:5000/');
+});
